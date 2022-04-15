5204712d23e6769beaed0cd0fbc04a5c046cc7f5
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-15 03:08:50 -0500

    New key functions II

--- src/main/java/com/banking/customer/service/app/service/imp/CustomerServiceImp.java
+++ src/main/java/com/banking/customer/service/app/service/imp/CustomerServiceImp.java
@@ -1,5 +1,59 @@
 package com.banking.customer.service.app.service.imp;
 
-public class CustomerServiceImp {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.banking.customer.service.app.model.Customer;
+import com.banking.customer.service.app.repository.CustomerRepository;
+import com.banking.customer.service.app.service.CustomerService;
+
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Service
+public class CustomerServiceImp implements CustomerService {
+	
+	@Autowired
+	private CustomerRepository customerRepository;
+	
+	@Override
+	public Flux<Customer> findAll() {
+		return customerRepository.findAll();
+	}
+
+	@Override
+	public Flux<Customer> findByLegalCustomer(Boolean legalCustomer) {
+		return customerRepository.findByLegalCustomer(legalCustomer);
+	}
+
+	@Override
+	public Flux<Customer> findByTributaryIdentifier(int tributaryIdentifier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Customer> findById(String id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Customer> findByPersonalIdentifier(int personalIdentifier) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Customer> save(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Mono<Void> delete(Customer customer) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
