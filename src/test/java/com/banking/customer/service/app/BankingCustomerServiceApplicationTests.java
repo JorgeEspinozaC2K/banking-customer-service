0bb8ca3d7266180bbad906201d6f0106cea6e0dd
Jorge Andres Espinoza Canales <jorge.espinoza.c22@gmail.com>
2022-04-15 00:44:06 -0500

    Initial commit 15-04-22

---
+++ src/test/java/com/banking/customer/service/app/BankingCustomerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.customer.service.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingCustomerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
